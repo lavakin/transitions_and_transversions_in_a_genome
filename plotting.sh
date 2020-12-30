82135609eb6e088ca41306792bd32e46fdef4f9b
kalabovanikola <nikola@kalabova.eu>
2020-12-30 15:02:31 +0100

    moving files

--- plotting.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-Rscript scripts/plots.R "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
--- sum.sh
+++
@@ -1,4 +0,0 @@
-> summary.txt
-echo "transitions	$(cut -f2 nucleotides.txt | paste -sd+ - | bc)" >> summary.txt
-echo "transversions	$(cut -f3 nucleotides.txt | paste -sd+ - | bc)" >> summary.txt
-
