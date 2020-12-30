de988256a42ade82a0f3bf4e20a2768074e7d211
kalabovanikola <nikola@kalabova.eu>
2020-12-30 01:22:10 +0100

    charts

--- nucleotides_ratio.sh
+++ nucleotides_ratio.sh
@@ -1,2 +1,6 @@
 bash count_trans.sh $1
 bash sum.sh
+cat nucleotides.txt | rs -c -C -T > transp_nucleotides.txt
+mkdir charts
+
+
--- sum.sh
+++ sum.sh
@@ -1,4 +1,4 @@
 > summary.txt
-echo "transitions	transversions" >> summary.txt
-echo "$(cut -f2 nucleotides.txt | paste -sd+ - | bc)	$(cut -f2 nucleotides.txt | paste -sd+ - | bc)" >> summary.txt
+echo "transitions	$(cut -f2 nucleotides.txt | paste -sd+ - | bc)" >> summary.txt
+echo "transversions	$(cut -f3 nucleotides.txt | paste -sd+ - | bc)" >> summary.txt
 
